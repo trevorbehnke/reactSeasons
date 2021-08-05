f2c85446fcefcb0aa9a259e3fb851c11ca34563c
trevor <me@trevorbehnke.com>
2021-08-05 14:21:25 -0700

    + display icons and text

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,9 +1,26 @@
 import React from "react";
 
-const SeasonDisplay = (props) => {
-  console.log(props.lat);
+const getSeason = (lat, month) => {
+  if (month > 2 && month < 9) {
+    return lat > 0 ? "summer" : "winter";
+  } else {
+    return lat > 0 ? "winter" : "summer";
+  }
+};
 
-  return <div>Season Display</div>;
+const SeasonDisplay = (props) => {
+  const season = getSeason(props.lat, new Date().getMonth());
+  const text =
+    season === "winter" ? "Burr, it's chilly!" : "Let's hit the beach!";
+  const icon = season === "winter" ? "snowflake" : "sun";
+
+  return (
+    <div>
+      <i className={`${icon} icon`}></i>
+      <h1>{text}</h1>
+      <i className={`${icon} icon`}></i>
+    </div>
+  );
 };
 
 export default SeasonDisplay;
